54f291e95443f711345dd835ea47802ebfb2cc00
Ilja Verba <ilja-verba00@yandex.ru>
2016-08-02 23:44:44 +0600

    Initial commit

---
+++ android/src/com/verba/game/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.verba.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.verba.game.testGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new testGame(), config);
+	}
+
+}
